86a1a3b06a12981b92b12779b179a2176bf62c2a
Liao CY <liaochungyid@gmail.com>
2021-10-12 22:56:04 +0800

    feat: add passport to authenticate login.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
+
 
 if (process.env.NODE_ENV !== 'produciton') {
   require('dotenv').config()
@@ -17,6 +19,14 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use(session({
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+const usePassport = require('./config/passport')
+usePassport(app)
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
 const db = require('../../models')
 const Todo = db.Todo
@@ -10,9 +11,10 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  res.send('login')
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
