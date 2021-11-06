12ada7aea63f776236325adb62aec7aafc77403d
Liao CY <liaochungyid@gmail.com>
2021-11-06 21:52:27 +0800

    feat: add message partials with flash message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
-
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'produciton') {
   require('dotenv').config()
@@ -28,9 +28,12 @@ app.use(session({
 const usePassport = require('./config/passport')
 usePassport(app)
 
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes)
