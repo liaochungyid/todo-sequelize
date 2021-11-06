af9544e1fedaf6fcbb8745b18ed93c4559c0b361
Liao CY <liaochungyid@gmail.com>
2021-11-06 22:14:49 +0800

    feat: add facebook login.

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -1,8 +1,15 @@
-module.exports = {
-  authenticator: (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    res.redirect('/users/login')
-  }
-}
+const express = require('express')
+const router = express.Router()
+
+const passport = require('passport')
+
+router.get('/facebook', passport.authenticate('facebook', {
+  scope: ['email', 'public_profile']
+}))
+
+router.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: 'users/login'
+}))
+
+module.exports = router
